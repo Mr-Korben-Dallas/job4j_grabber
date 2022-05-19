28365edb90115068dd5abffa282c6e8937fbc19c
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-05-19 12:20:32 +0400

    [#260360]: - implemented logic that makes a record in the database, before and after job was executed;

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,6 +4,8 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -12,30 +14,51 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
     private static final String PROPERTIES_FILE = "rabbit.properties";
-    private static final String SCHEDULE_TIME_INTERVAL_IN_SECONDS = "rabbit.interval";
+    private static final String PROPERTY_SCHEDULE_TIME_INTERVAL_IN_SECONDS = "rabbit.interval";
+    private static final String PROPERTY_DB_URL = "url";
+    private static final String PROPERTY_DB_USERNAME = "username";
+    private static final String PROPERTY_DB_PASSWORD = "password";
+    private static final String PROPERTY_DB_DRIVER_CLASS_NAME = "driver-class-name";
+    private static final String JOB_DATA_MAP_KEY_CONNECTION = "connection";
+    private static final String QUERY_INSERT_CREATED_DATE = "insert into rabbit (created_date) values (?)";
 
     public static void main(String[] args) {
-        try {
+        try (Connection connection = getConnection()) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap jobDataMap = new JobDataMap();
+            jobDataMap.put(JOB_DATA_MAP_KEY_CONNECTION, connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(jobDataMap)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(getProperty(SCHEDULE_TIME_INTERVAL_IN_SECONDS)))
+                    .withIntervalInSeconds(Integer.parseInt(getProperty(PROPERTY_SCHEDULE_TIME_INTERVAL_IN_SECONDS)))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ...");
+            makeCreatedDateRecord((Connection) context.getJobDetail().getJobDataMap().get(JOB_DATA_MAP_KEY_CONNECTION));
+        }
+
+        private void makeCreatedDateRecord(Connection connection) {
+            try (PreparedStatement ps = connection.prepareStatement(QUERY_INSERT_CREATED_DATE)) {
+                ps.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                ps.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -47,5 +70,20 @@ public class AlertRabbit {
             e.printStackTrace();
         }
         return properties.getProperty(keyOfProperty);
+    }
+
+    private static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(getProperty(PROPERTY_DB_DRIVER_CLASS_NAME));
+            connection = DriverManager.getConnection(
+                    getProperty(PROPERTY_DB_URL),
+                    getProperty(PROPERTY_DB_USERNAME),
+                    getProperty(PROPERTY_DB_PASSWORD)
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
