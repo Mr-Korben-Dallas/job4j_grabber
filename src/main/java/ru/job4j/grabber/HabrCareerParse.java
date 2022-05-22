3c43f14733ce539662f1f4fa884a271357cd4ed6
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-05-22 11:47:38 +0400

    [#285212]: - implemented logic for getting vacancy details;

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -12,6 +12,12 @@ public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
     private static final String NEXT_PAGE_LINK = String.format("%s?page=", PAGE_LINK);
+    private static final String VACANCY_CARD_ELEMENT_MAIN = ".vacancy-card__inner";
+    private static final String VACANCY_CARD_ELEMENT_TITLE = ".vacancy-card__title";
+    private static final String VACANCY_CARD_ELEMENT_TITLE_ATTR_HREF = "href";
+    private static final String VACANCY_CARD_ELEMENT_DATE = ".vacancy-card__date";
+    private static final String VACANCY_CARD_ELEMENT_DATE_ATTR_DATETIME = "datetime";
+    private static final String VACANCY_CARD_DESCRIPTION = ".job_show_description__vacancy_description";
     private static final int NUMBER_OF_PAGES_TO_PARSE = 5;
 
     public static void main(String[] args) {
@@ -21,22 +27,33 @@ public class HabrCareerParse {
     }
 
     private static void parse(int pageNumber) {
+        Document document = getPageDocument(NEXT_PAGE_LINK + pageNumber);
+        Elements rows = document.select(VACANCY_CARD_ELEMENT_MAIN);
+        rows.forEach(row -> {
+            Element titleElement = row.select(VACANCY_CARD_ELEMENT_TITLE).first();
+            Element linkElement = titleElement.child(0);
+            Element dateTimeElement = row.select(VACANCY_CARD_ELEMENT_DATE).first();
+            Element timeElement = dateTimeElement.child(0);
+            String dateTimeValue = timeElement.attr(VACANCY_CARD_ELEMENT_DATE_ATTR_DATETIME);
+            String vacancyName = titleElement.text();
+            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr(VACANCY_CARD_ELEMENT_TITLE_ATTR_HREF));
+            System.out.printf("%s %s %s%n", dateTimeValue, vacancyName, link);
+        });
+    }
+
+    private static Document getPageDocument(String url) {
+        Document document = null;
         try {
-            Connection connection = Jsoup.connect(NEXT_PAGE_LINK + pageNumber);
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                Element dateTimeElement = row.select(".vacancy-card__date").first();
-                Element timeElement = dateTimeElement.child(0);
-                String dateTimeValue = timeElement.attr("datetime");
-                String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", dateTimeValue, vacancyName, link);
-            });
-        } catch (IOException e) {
+            Connection connection = Jsoup.connect(url);
+            document = connection.get();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        return document;
+    }
+
+    private static String retrieveDescription(String url) {
+        Document document = getPageDocument(url);
+        return document.select(VACANCY_CARD_DESCRIPTION).text();
     }
 }
